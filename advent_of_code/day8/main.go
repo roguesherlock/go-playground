36570a2191650887bb53cc7fb87993c3d411aefa
Akash <aakash@hey.com>
2022-12-12 16:10:18 +0530

    day 8 part 2

--- advent_of_code/day8/main.go
+++ advent_of_code/day8/main.go
@@ -79,12 +79,57 @@ func main() {
 		return visible
 	}
 
+	calculateScenicScore := func(grid [][]int, i, j int) int {
+		// look left
+		leftScore := 0
+		for k := j - 1; k >= 0; k-- {
+			leftScore++
+			if grid[i][k] >= grid[i][j] {
+				break
+			}
+		}
+
+		// look right
+		rightScore := 0
+		for k := j + 1; k < len(grid[i]); k++ {
+			rightScore++
+			if grid[i][k] >= grid[i][j] {
+				break
+			}
+		}
+
+		// look up
+		upScore := 0
+		for k := i - 1; k >= 0; k-- {
+			upScore++
+			if grid[k][j] >= grid[i][j] {
+				break
+			}
+		}
+
+		// look down
+		downScore := 0
+		for k := i + 1; k < len(grid); k++ {
+			downScore++
+			if grid[k][j] >= grid[i][j] {
+				break
+			}
+		}
+		return leftScore * rightScore * upScore * downScore
+	}
+
+	maxScenicScore := 0
 	for i := 1; i < len(grid)-1; i++ {
 		for j := 1; j < len(grid[i])-1; j++ {
 			if isVisible(grid, i, j) {
 				visibleTrees++
 			}
+			score := calculateScenicScore(grid, i, j)
+			if score > maxScenicScore {
+				maxScenicScore = score
+			}
 		}
 	}
 	log.Println("visible trees:", visibleTrees)
+	log.Println("highest scenic score:", maxScenicScore)
 }
